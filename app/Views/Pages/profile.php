3b98b35b3b125b3dad1002a09aad2a4b1c8a7b1c
Randy <casongayr@gmail.com>
2025-11-03 01:52:52 +0800

    Page/Profile: added html contents

--- app/Views/Pages/profile.php
+++ app/Views/Pages/profile.php
@@ -7,6 +7,78 @@
 </head>
 <body>
     <h1>This is the Profile Page</h1>
-    
+    <br>
+    <div class="profile">
+        <h1>Profile</h1>
+        <a href="" title="profile"></a>
+        <form>
+            <div class="profile-info">
+                <div class="" value="fname">FirstName</div>
+                <div class="" value="lname">LastName</div>
+                <div class="" value="tel">Telephone</div>
+                <div class="" value="email">Email</div>
+            </div>
+            <br>
+            <div>
+                <button>Change Password</button>
+                <button>Log out</button>
+            </div>
+            <br>
+            <br>
+            <div class="user-dashboard">
+                <h1>Hey customer! Thanks for being with us</h1>
+                <div class="" value="tp">Total Purchases:</div>
+                <div class="" value="sc">Stamps Claimed:</div>
+                <div class="" value="lod">Last Order Date:</div>
+            </div>
+            <br>
+            <br>
+            <div class="order-history">
+                <h1>Order History</h1>
+                <br>
+                <div class="item">
+                    <h2>Coffee1</h2>
+                    <h2>09/29/25 5:30 PM</h2>
+                    <h2>Paid</h2>
+                    <h2>Stamps Claimed</h2>
+                </div>
+                <br>
+                <div class="item">
+                    <h2>Coffee2</h2>
+                    <h2>09/29/25 5:30 PM</h2>
+                    <h2>Paid</h2>
+                    <h2>Stamps Not Claimed</h2>
+                </div> 
+                <br>
+                <div class="item">
+                    <h2>Coffee3</h2>
+                    <h2>09/29/25 5:30 PM</h2>
+                    <h2>Paid</h2>
+                    <h2>Stamps Not Claimed</h2>
+                </div> 
+                <br>
+                <div class="item">
+                    <h2>Coffee4</h2>
+                    <h2>09/29/25 5:30 PM</h2>
+                    <h2>Paid</h2>
+                    <h2>Stamps Claimed</h2>
+                </div> 
+                <br>
+                <div class="item">
+                    <h2>Coffee1</h2>
+                    <h2>09/29/25 5:30 PM</h2>
+                    <h2>Paid</h2>
+                    <h2>Stamps Claimed</h2>
+                </div> 
+                <br>
+                <div class="item">
+                    <h2>Coffee1</h2>
+                    <h2>09/29/25 5:30 PM</h2>
+                    <h2>Paid</h2>
+                    <h2>Stamps Claimed</h2>
+                </div>  
+            </div>
+        </form>
+    </div>
 </body>
 </html>
