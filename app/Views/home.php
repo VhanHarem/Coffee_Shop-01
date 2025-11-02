875f4281d5bf58f0db3f6cda688b28c52bdf6384
Frtzzz-zz <superidoruno22@gmail.com>
2025-11-02 22:12:00 +0800

    added tailwind in src folder

--- app/Views/home.php
+++ app/Views/home.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="/public/src/tailwindstyles.css" rel="stylesheet">    
 </head>
 <body>
-    <p class = "c"> i love codeigniter </p>
+    <p class = "c bg-amber-500 p-5 rounded-md w-5 h-5"> i love codeigniter </p>
     <a href="<?= site_url('categories') ?>">click</a>
 
 </body>
