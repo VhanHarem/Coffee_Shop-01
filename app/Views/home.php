223c2647dcdff9e1cf963ff1743d2622c65c6ce3
Vhan <vergara_vhanharem@spcc.edu.phgit config -- global user.name>
2025-11-02 23:27:49 +0800

    inayos ko na

--- app/Views/home.php
+++ app/Views/home.php
@@ -7,8 +7,9 @@
     <link href="../public/src/tailwindstyles.css" rel="stylesheet">    
 </head>
 <body>
-    <p class = "c"> i love codeigniter </p>
+    <h1 class = "text-red-300">RANDY WAG MO SIRAIN!</h1>
     <a href="<?= site_url('categories') ?>">click</a>
-    <div class="bg-amber-500 p-1 rounded-md w-[50px] h-[50px]"></div>
+    <div class="bg-amber-300 p-1 rounded-md w-[50px] h-[50px]"></div>
+     <div class="bg-amber-300 p-1 rounded-md w-[50px] h-[50px]"></div>
 </body>
 </html>
