fef9959712b2cf2497e7f6d74012f471dbff02ea
Vhan <vergara_vhanharem@spcc.edu.phgit config -- global user.name>
2025-11-02 21:36:02 +0800

    added some stuff

--- app/Views/categories.php
+++ app/Views/categories.php
@@ -8,5 +8,19 @@
 <body>
     <h1>Oct 16 11:23pm<h1>
     <h1>PORTUGAL  INCIDENT</h1>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6VQQHL2AM8p-Q-bH-ZO39bq1fsiagkrXbUA&s" alt="hehe">
+    <style>
+        h1{
+            text-align: center;
+            color: red;
+        }
+
+        img{
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 50%;
+        }
+    </style>
 </body>
 </html>
