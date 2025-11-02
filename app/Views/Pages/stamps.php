7e45e24bfbb877e5bf3f61e3209ac36a1aeb487c
Frtzzz-zz <superidoruno22@gmail.com>
2025-11-03 01:06:24 +0800

    stamps skeleton

--- app/Views/Pages/stamps.php
+++ app/Views/Pages/stamps.php
@@ -4,8 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Stamps</title>
+    <link href="../../../../Coffee_Shop-01/public/src/tailwindstyles.css" rel="stylesheet">
 </head>
 <body>
-    <h1>This is the Stamps Page</h1>
+    <h1 class="">This is the Stamps Page</h1>
+
+    <div id="stamps" class="grid grid-cols-4 gap-4 mb-4">
+                
+        <div class="" value="stamped">Stamp 1</div>
+        <div class="" value="stamped">Stamp 2</div>
+        <div class="" value="stamped">Stamp 3</div>
+        <div class="" value="default">Stamp 4</div>
+        <div class="" value="default">Stamp 5</div>
+        <div class="" value="default">Stamp 6</div>
+        <div class="" value="stamped">Stamp 7</div>
+        <div class="" value="stamped">Stamp 8</div>
+        <div class="" value="stamped">Stamp 9</div>
+        <div class="" value="default">Stamp 10</div>
+        
+    </div> 
+
 </body>
+
+
 </html>
