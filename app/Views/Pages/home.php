7583ab8a4f0b10104d9cf19313bfeec00e7e38a3
Vhan <vergara_vhanharem@spcc.edu.phgit config -- global user.name>
2025-11-03 01:36:19 +0800

    home update

--- app/Views/Pages/home.php
+++ app/Views/Pages/home.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Home</title>
+  <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
   <!DOCTYPE html>
@@ -29,8 +30,14 @@
 
   <!-- Body -->
   <div class="body">
-    <h1>Randyy Wagggg!!! Jukopoo</h1>
+    <h1>BEST SELLERS</h1>
     <p>This is the main content area.</p>
+    <div class="flex flex-wragap-4 items-center" id = "Products">
+      <div id = "Product_1" class=""><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgqpEjWZM4UwJMAWrJkeSuyhnrWYfHvMdIKQ&s" alt="coffee"></div>
+      <div id = "Product_2" class=""><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgqpEjWZM4UwJMAWrJkeSuyhnrWYfHvMdIKQ&s" alt="coffee"></div>
+      <div id = "Product_3" class=""><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgqpEjWZM4UwJMAWrJkeSuyhnrWYfHvMdIKQ&s" alt="coffee"></div>
+      <div id = "Product_4" class=""><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgqpEjWZM4UwJMAWrJkeSuyhnrWYfHvMdIKQ&s" alt="coffee"></div>
+    </div>
   </div>
 
   <!-- Footer -->
