1482ea623c26710a03b9b6a546d539a9ceaf3a02
Randy <casongayr@gmail.com>
2025-11-03 00:51:19 +0800

    Pages/login: added html content

--- app/Views/Pages/login.php
+++ app/Views/Pages/login.php
@@ -6,7 +6,38 @@
     <title>Login</title>
 </head>
 <body>
-    <h1>This is the Login Page</h1>
-    
+    <div class="login-container">
+        <h1>Login</h1>
+        <form action="/login" method="post">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+            <br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+            <br>
+            <br>
+            <button type="submit">Login</button>
+            <button>Cancel</button>
+        </form>
+    </div>
+    <div class="Signup-container">
+        <h1>Sign up</h1>
+        <form method="post">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+            <br>
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email">
+            <br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+            <br>
+            <label for="confirmpass">Confirm Password:</label>
+            <input type="password" id="confirmpass" name="confirmpass" required>
+            <br>
+            <button type="submit">Sign up</button>
+            <button>Cancel</button>
+        </form>
+    </div>
 </body>
 </html>
