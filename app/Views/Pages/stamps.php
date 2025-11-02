421462bb791a3de3ced11021e03b3ff35e830a51
Frtzzz-zz <superidoruno22@gmail.com>
2025-11-03 01:07:45 +0800

    navbar sa stamps

--- app/Views/Pages/stamps.php
+++ app/Views/Pages/stamps.php
@@ -7,6 +7,18 @@
     <link href="../../../../Coffee_Shop-01/public/src/tailwindstyles.css" rel="stylesheet">
 </head>
 <body>
+    <div class="navbar">
+        <div class="logo">MyLogo</div>
+        <div class="nav-links">
+            <a href="<?= site_url('home') ?>">Home</a>
+                <a href="<?= site_url('categories') ?>">Categories</a>
+                <a href="<?= site_url('orders') ?>">Orders</a>
+                <a href="<?= site_url('profile') ?>">Profile</a>
+                <a href="<?= site_url('stamps') ?>">Stamps</a>
+            <a href="<?= site_url('profile') ?>">Profile</a>
+        </div>
+    </div>
+
     <h1 class="">This is the Stamps Page</h1>
 
     <div id="stamps" class="grid grid-cols-4 gap-4 mb-4">
